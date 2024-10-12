5636e6b715352b542f387c60c18f6de4c20e3e10
RainielJosephSicat <rainieljosephsicat@gmail.com>
2024-10-11 19:26:00 -0700

    commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    template_name = 'app/home.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'app/about.html'
+# Create your views here.
